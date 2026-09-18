00e2fbed84c883b3cbcb001cedcace1729403404
aivideomaking12-maker <aivideomaking12@gmail.com>
2026-09-18 10:51:33 +0200

    Update app.js

--- js/app.js
+++ js/app.js
@@ -11,7 +11,6 @@ window.App = (function() {
      * Initializes the app on page load
      */
     async function init() {
-        if (window.Navigation && window.Navigation.initTheme) window.Navigation.initTheme();
         console.log("CyberShield program inicializálása...");
         
         // Update UI status banner based on Supabase Connection
